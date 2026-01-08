54f67c2856beda550fccdf35b560cde395766827
Andrew Hannigan <andrew.s.hannigan@gmail.com>
2026-01-08 00:11:03 -0600

    fixes

---
+++ scripts/notarize.js
@@ -0,0 +1,30 @@
+const { notarize } = require('@electron/notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  // Skip notarization if credentials aren't set (local builds)
+  if (!process.env.APPLE_ID || !process.env.APPLE_APP_SPECIFIC_PASSWORD || !process.env.APPLE_TEAM_ID) {
+    console.log('Skipping notarization: credentials not set');
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+  const appBundleId = context.packager.appInfo.id;
+  const appPath = `${appOutDir}/${appName}.app`;
+
+  console.log(`Notarizing ${appBundleId} at ${appPath}...`);
+
+  await notarize({
+    appBundleId,
+    appPath,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  });
+
+  console.log('Notarization complete');
+};
